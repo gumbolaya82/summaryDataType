04a5494104912f54005a12c04a0ce3ec81d3b367
gumbolaya82 <106184686+gumbolaya82@users.noreply.github.com>
2022-05-26 14:35:46 -0400

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,8 +1,8 @@
 /**
  * class name: Variables.java
- * author: ________
- * date:
- * version:
+ * author: Tommy Hoang
+ * date: 05/26/2022
+ * version: 1.0
  * description: This program will display summary of primitive data types.
  * source: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
  * direction: 1) read the source material
@@ -15,6 +15,22 @@ public class Variables
     public static void main(String[] args)
     {
         System.out.println("Summary of The Primitive Data Types");
+        System.out.println("Byte is a 8 bit complement integer that is potentially useful for saving memory in large arrays");
+        System.out.println("The value can range between -128 and 127 and can substitute an int");
+        System.out.println("Short is a 16 bit complement integer that can range between -32,768 and 32,767");
+        System.out.println("It's similar to a byte as you can use it to save memory in large arrays");
+        System.out.println("A int is a 32 bit complement integer that can range between -2^31 and (2^32)-1");
+        System.out.println("A long is a 64 bit complement integer that can range between -2^63 and (2^63)-1");
+        System.out.println("You can utilize this data type to show an unsigned 64-bit long which ranges from 0 and (2^64)-1");
+        System.out.println("The float is a single-precision 32-bit IEEE 754 floating point");
+        System.out.println("It shouldn't be used for precise values");
+        System.out.println("A double is a double precision compared to float");
+        System.out.println("Usually the preferred choice but still should not be used for precise values");
+        System.out.println("A boolean is only true or false");
+        System.out.println("Useful for questions that only have true or false as outputs");
+        System.out.println("A char is a 16 bit unicode character");
+        System.out.println("Ranges from 0 to 65,535");
+        
     }
 
 }
